b1eb1708ad70823e043ff5c49ecb23bd87b37ab8
Nexxxit <varsik.arsik@mail.ru>
2025-02-18 19:23:02 +0500

    add Profile.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import Profile from "./components/profile/Profile";
+
 function App() {
   return (
-    <>
-    </>
-  )
+    <div className="flex flex-col px-[25px]">
+      <Profile />
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,6 +1,32 @@
+import { useState } from "react";
+
 export default function Profile() {
-    return(
-        <>
-        </>
-    );
+    const [userData, setUserData] = useState({
+        first_name: "Имя",
+        last_name: "Фамилия",
+        username: "@guest",
+    });
+
+    const [photoUrl, setPhotoUrl] = useState(null);
+
+  return (
+    <>
+      <div className="flex gap-4 items-center py-4">
+        {photoUrl ? (
+            <img
+            className="shrink-0 aspect-square size-24 rounded-[36px] object-cover"
+            src={photoUrl}
+            alt="Фото профиля"
+          />
+        ) : (
+            <div className="shrink-0 aspect-square size-24 rounded-[36px] bg-gray-200 flex items-center justify-center">
+                <span className="text-gray-500">
+                    Нет фото
+                </span>
+            </div>
+        )}
+        <p className="text-3xl">{`${userData.first_name} ${userData.last_name}`}</p>
+      </div>
+    </>
+  );
 }
